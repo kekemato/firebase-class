8ae80cf302276b0606cc14ea195f423e2898c9b2
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-26 15:04:45 +0100

    end of classes for today

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { database } from './firebase'
 
 class App extends Component {
+
+  componentDidMount() {
+    database.ref('/kulturalne')
+      .on('value', (snapshot) => {
+        const firebaseData = Object.entries(
+          snapshot.val() || {}
+        )
+
+        const data = firebaseData.map(([id, value]) => {
+          // value.id = id;
+          return value;
+        })
+        console.log(data)
+      })
+    const newEvent = {
+      name: "Les Miserables",
+      date: "05.12.2018",
+      description: "american musical based on french story"
+    }
+
+    database.ref('/events').push(newEvent)
+  }
+
   render() {
     return (
       <div className="App">
@@ -21,8 +45,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
